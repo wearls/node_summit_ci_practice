fb95b9c533f50a002a54ec2b991a04e21925186f
wearls <william.earls@priceline.com>
2015-02-09 19:58:19 -0500

    outputting port

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Universe\n');
+  res.end('Hello Universe\n' + process.env.PORT);
 }).listen(process.env.PORT || 3000, '127.0.0.1');
 console.log('running');
+
+
